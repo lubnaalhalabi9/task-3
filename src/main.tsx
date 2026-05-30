baca2ad64888efd86a611f1c1e08a725cadd180d
Lubna Alhalabi <lubnaalhalabi5@gmail.com>
2026-05-30 22:52:11 +0300

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -10,41 +10,43 @@ import Admission from './pages/Admission'
 import StudentLife from './pages/StudentLife'
 import Contact from './pages/Contact'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          path: "academics",
+          element: <Academics />,
+        },
+        {
+          path: "admission",
+          element: <Admission />,
+        },
+        {
+          path: "student-life",
+          element: <StudentLife />,
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
   {
-    path : "/",
-    element :<Root />,
-    children : [
-      {
-        path :"", 
-        element :<Home /> 
-      },
-      {
-        path :"about", 
-        element : <About />
-      },
-      {
-        path :"academics", 
-        element : <Academics />
-      },
-      {
-        path :"admission", 
-        element : <Admission />
-      },
-      {
-        path :"student-life", 
-        element : <StudentLife />
-      },
-      {
-        path :"contact", 
-        element : <Contact />
-      },
-    ]
+    basename: "/task-3",
   }
-],
-{
-  basename: "/task-3"
-})
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/task-3/",
   plugins: [react()],
+  base: "/task-3/"
 })
